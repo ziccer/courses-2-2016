7e032203993de64808dcf0498c8738f09001d748
Ievgen Tararaka <itararaka@playtika.com>
2016-07-08 17:42:02 +0300

    Add method for reading from console

--- module3/src/main/java/com/courses/spalah/Calculator.java
+++ module3/src/main/java/com/courses/spalah/Calculator.java
@@ -1,10 +1,24 @@
 package com.courses.spalah;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * @author Ievgen Tararaka
  */
 public class Calculator {
     public static void main(String[] args) {
         System.out.println("Start calculator");
+    }
+
+    public static String[] readFromConsole() {
+        try {
+            BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
+            String input = bufferRead.readLine();
+            return input.split("\\s");
+        } catch (IOException e) {
+            return new String[]{""};
+        }
     }
 }
